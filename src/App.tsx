5d2e440c7de233b9723fa11866612100046c1455
Dwan <zaishuiyiduang@outlook.com>
2024-02-26 09:32:02 +0800

    basic configure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import { Index } from "./pages";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-primary">Hello, Vite + React + TS + TailwindCSS</h1>
+      <Index />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Button, Card } from "@/components";
+
+export const Index = () => {
+  return (
+    <>
+      <p className="container bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Hello, Index;
+      </p>
+      <Button />
+      <Card />
+    </>
+  );
+};
