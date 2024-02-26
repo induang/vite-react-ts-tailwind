41e93794a6657ae4b597e93b9adb591d9a2300ae
Dwan <zaishuiyiduang@outlook.com>
2024-02-26 12:02:37 +0800

    vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: "dist/stats.html",
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
+  build: {
+    sourcemap: false,
+    minify: "esbuild",
+    rollupOptions: {
+      output: {
+        compact: true,
+        chunkFileNames: "assets/[name]-[hash].js",
+        manualChunks: {
+          react: ["react", "react-dom"], // react-router-dom
+          // "react-state": ["react-hook-form", "@tanstack/react-query", "@reduxjs/toolkit"],
+          // "ui-vendors": [""],
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
